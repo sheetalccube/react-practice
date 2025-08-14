66213e1794b6cdc91108e2a21f355d71110b4c13
sheetal sonsale <sheetal.s@ccube.com>
2025-08-14 16:21:35 +0530

    apply theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import './styles/variables.css';
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import theme from "./theme";
 
 import {  RouterProvider } from "react-router-dom";
 import { router } from "./router.tsx";
 
 createRoot(document.getElementById("root")!).render(
   // <BrowserRouter>
-  <RouterProvider router={router}/>
   // </BrowserRouter>
+  <ThemeProvider theme={theme}>
+    <CssBaseline /> {/* Resets & normalizes styles */}
+    <RouterProvider router={router}/>
+  </ThemeProvider>
+
 );
---
+++ src/theme.ts
@@ -0,0 +1,37 @@
+import { createTheme } from "@mui/material/styles";
+
+// Create a custom theme with full TypeScript support
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#19d25dff", // blue
+    },
+    secondary: {
+      main: "#2730b0ff", // purple
+    },
+    background: {
+      default: "#f5f5f5", // light grey background
+    },
+  },
+  typography: {
+    fontFamily: "'Roboto', sans-serif",
+    h1: {
+      fontSize: "2rem",
+      fontWeight: 700,
+    },
+    button: {
+      textTransform: "none", // removes uppercase
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: "8px",
+        },
+      },
+    },
+  },
+});
+
+export default theme;
